576c9e54b3f6a794a5d6f8b5cfdd17730c784f46
Iryna <iradil373@gmail.com>
2026-03-11 18:44:47 +0100

    add js

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    ///* * Принимает три параметра:
+    * - ширину контента
+    * - ширину padding
+    * - ширину border
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
