6bad99f1a87c445ec4b6096e1e2a030af7db4abc
Iryna <iradil373@gmail.com>
2026-03-14 11:09:34 +0100

    change js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
- ///*  * Принимает два параметра:
-  * - количество дроидов, которые хочет купить клиент
-  * - цену одного дроида
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
